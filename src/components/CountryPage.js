03f997075dea6ae6ca92f7d314405fff5cf260e6
VasuBeachoo <vasubeachoo@gmail.com>
2022-10-26 15:27:07 -0400

    Style country page desktop layout

--- src/components/CountryPage.js
+++ src/components/CountryPage.js
@@ -1,23 +1,68 @@
 import styled from "styled-components";
 import CountryInfoItem from "./CountryInfoItem";
 
-export const BorderCountriesList = styled.div``;
+export const BorderCountriesList = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+`;
 
 export const BorderCountriesLabel = styled.h2``;
 
-export const BorderCountriesBox = styled.div``;
+export const BorderCountriesBox = styled.div`
+  grid-area: 3 / 1 / span 1 / span 2;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+`;
 
-export const ExtraInfoBox = styled.div``;
+export const ExtraInfoBox = styled.div`
+  grid-area: 2 / 2 / span 1 / span 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
 
-export const CountryName = styled.h1``;
+export const MainInfoBox = styled.div`
+  grid-area: 2 / 1 / span 1 / span 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
 
-export const MainInfoBox = styled.div``;
+export const CountryName = styled.h1`
+  grid-area: 1 / 1 / span 1 / span 2;
+`;
 
-export const CountryFlag = styled.img``;
+export const InfoItemsBox = styled.div`
+  display: grid;
+  grid-template: repeat(3, auto) / repeat(2, auto);
+`;
+
+export const CountryFlag = styled.img`
+  width: 100%;
+  max-width: 40rem;
+`;
+
+export const CountryInfoBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
 
 export const BackBtn = styled.button``;
 
-export const CountryPageBox = styled.div``;
+export const CountryPageBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
 
 const CountryPage = ({ className, info }) => {
   const displayListNames = (list) => {
@@ -30,36 +75,40 @@ const CountryPage = ({ className, info }) => {
     info && (
       <CountryPageBox className={className}>
         <BackBtn>Back</BackBtn>
-        <CountryFlag src={info.flags.svg} alt={`${info.name} flag`} />
-        <MainInfoBox>
-          <CountryName>{info.name}</CountryName>
-          <CountryInfoItem label="Native Name" value={info.nativeName} />
-          <CountryInfoItem
-            label="Population"
-            value={info.population.toLocaleString()}
-          />
-          <CountryInfoItem label="Region" value={info.region} />
-          <CountryInfoItem label="Sub Region" value={info.subregion} />
-          <CountryInfoItem label="Capital" value={info.capital} />
-        </MainInfoBox>
-        <ExtraInfoBox>
-          <CountryInfoItem
-            label="Top Level Domain"
-            value={info.topLevelDomain[0]}
-          />
-          <CountryInfoItem
-            label="Currencies"
-            value={displayListNames(info.currencies)}
-          />
-          <CountryInfoItem
-            label="Languages"
-            value={displayListNames(info.languages)}
-          />
-        </ExtraInfoBox>
-        <BorderCountriesBox>
-          <BorderCountriesLabel>Border Countries:</BorderCountriesLabel>
-          <BorderCountriesList></BorderCountriesList>
-        </BorderCountriesBox>
+        <CountryInfoBox>
+          <CountryFlag src={info.flags.svg} alt={`${info.name} flag`} />
+          <InfoItemsBox>
+            <CountryName>{info.name}</CountryName>
+            <MainInfoBox>
+              <CountryInfoItem label="Native Name" value={info.nativeName} />
+              <CountryInfoItem
+                label="Population"
+                value={info.population.toLocaleString()}
+              />
+              <CountryInfoItem label="Region" value={info.region} />
+              <CountryInfoItem label="Sub Region" value={info.subregion} />
+              <CountryInfoItem label="Capital" value={info.capital} />
+            </MainInfoBox>
+            <ExtraInfoBox>
+              <CountryInfoItem
+                label="Top Level Domain"
+                value={info.topLevelDomain[0]}
+              />
+              <CountryInfoItem
+                label="Currencies"
+                value={displayListNames(info.currencies)}
+              />
+              <CountryInfoItem
+                label="Languages"
+                value={displayListNames(info.languages)}
+              />
+            </ExtraInfoBox>
+            <BorderCountriesBox>
+              <BorderCountriesLabel>Border Countries:</BorderCountriesLabel>
+              <BorderCountriesList></BorderCountriesList>
+            </BorderCountriesBox>
+          </InfoItemsBox>
+        </CountryInfoBox>
       </CountryPageBox>
     )
   );
